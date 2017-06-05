43d4f490b75c2369c8769d511ac992f61a7402ac
Steve Piner <steve.piner@gmail.com>
2017-06-05 14:50:35 +1200

    Add diff script
    Determine the differences between the repository and the installed
    version of the files.

---
+++ diff.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# Created on 5/06/2017 by Steve Piner
+use v5.22;
+use strict;
+use warnings;
+use utf8;
+use autodie;
+use version; our $VERSION = qv('0.0.1');
+
+use File::Basename qw[dirname basename];
+use Cwd qw[realpath];
+
+my $dir = dirname($0) . '/home';
+my $dh;
+opendir $dh, $dir;
+
+while (my $file = readdir $dh) {
+    next if $file =~ /^\.\.?$/;
+    my @args = ('diff', @ARGV, "$dir/$file", "$ENV{HOME}/$file");
+    say "@args";
+    system @args;
+}
+closedir $dh;
